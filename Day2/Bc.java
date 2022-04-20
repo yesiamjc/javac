1a2d5225b1fce067c6dcb5f4e489f0ca49ceb4ab
Joy Chakraborty <chakrabortyjoy638@gmail.com>
2022-04-20 23:55:13 +0530

    Day 2 ka Code

---
+++ Day2/Bc.java
@@ -0,0 +1,5 @@
+public class Bc{
+    public static void main(String args[]){
+        System.out.println(2+2);
+    }
+}
---
+++ Day2/Mc.java
@@ -0,0 +1,6 @@
+public class Mc {
+    public static void main(String args[]){
+        System.out.print("Difference between these two ");
+        System.out.print("Difference between these two ");
+    }
+}
